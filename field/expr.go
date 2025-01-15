e3abb39be33e5c75ec9d79ae9198137d9f6faa6f
bomb-bomb <56525455+bomb-bomb@users.noreply.github.com>
2025-01-15 15:25:24 +0800

    Update expr.go

--- field/expr.go
+++ field/expr.go
@@ -434,8 +434,8 @@ func (e expr) DateValueBetweenCol(value interface{}, col Expr) expr {
 	return e.setE(clause.Expr{SQL: "? between ? and ? + INTERVAL '1 days' * ?", Vars: []interface{}{value, e.RawExpr(), e.RawExpr(), col.RawExpr()}})
 }
 
-func (e expr) IfNullCol(col Expr) expr {
-	return e.setE(clause.Expr{SQL: "IFNULL(?,?)", Vars: []interface{}{e.RawExpr(), col.RawExpr()}})
+func (e expr) Coalesce(col Expr) expr {
+	return e.setE(clause.Expr{SQL: "COALESCE(?,?)", Vars: []interface{}{e.RawExpr(), col.RawExpr()}})
 }
 
 func (e expr) Include(value interface{}) expr {
